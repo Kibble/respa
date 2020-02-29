4184180b3a4aa25ec6ac4f979070e4db3da42add
Fu Thong <kibble.bytes08@gmail.com>
2020-02-29 15:57:29 -0500

    Added react-router-dom for routing.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import NavBar from './modules/layout/component/NavBar.component';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import News from './modules/app/component/News.component';
 
 function App() {
-    return <div className="App">
-        <NavBar />
-        <div className="app-content">
-            <img src={logo} className="App-logo" alt="logo" />
+    return <BrowserRouter>
+        <div className="App">
+            <NavBar />
+            <Switch>
+                <Route path="/news">
+                    <News />
+                </Route>
+                <Route path="/">
+                    <div className="app-content">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </div>
+                </Route>
+            </Switch>
         </div>
-    </div>;
+    </BrowserRouter>;
 }
 
 export default App;
--- src/modules/layout/component/NavBar.component.js
+++ src/modules/layout/component/NavBar.component.js
@@ -1,12 +1,13 @@
 import {Button, Toolbar} from 'react-md';
 import React from 'react';
 import '../style/layout.scss';
+import {Link} from 'react-router-dom';
 
 const NavBar = () => <Toolbar className="app-toolbar" colored>
-    <Button className="app-toolbar__page-button" flat>Home</Button>
-    <Button className="app-toolbar__page-button" flat>News</Button>
-    <Button className="app-toolbar__page-button" flat>Gallery</Button>
-    <Button className="app-toolbar__page-button" flat>About</Button>
+    <Link to="/"><Button className="app-toolbar__page-button" flat>Home</Button></Link>
+    <Link to="/news"><Button className="app-toolbar__page-button" flat>News</Button></Link>
+    <Link to="/"><Button className="app-toolbar__page-button" flat>Gallery</Button></Link>
+    <Link to="/"><Button className="app-toolbar__page-button" flat>About</Button></Link>
 </Toolbar>;
 
 export default NavBar;
